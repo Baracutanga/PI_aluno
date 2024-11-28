1ab95d2c91eac406fd3acb93574224999bedfbb1
Alexandre Machado <156339431+alexandreM12@users.noreply.github.com>
2024-11-28 00:21:27 -0300

    Tela de Perfil - Pronta

--- app/Perfil/Perfil.jsx
+++ app/Perfil/Perfil.jsx
@@ -1,58 +1,124 @@
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Footer from "../../src/components/Footer/Footer";
-const Perfil = () => {  
+
+const Perfil = () => {
   const usuario = {
     nome: "Rodrigo",
     senha: "12345",
     email: "rodrigo@email.com",
   };
-  const senhaSegura = usuario.senha.split("").map(() => "*").join("");
+  const senhaSegura = usuario.senha
+    .split("")
+    .map(() => "*")
+    .join("");
 
   return (
     <SafeAreaProvider>
-      <View>
-        {/* Perfil = Nome e imag */}
-        <View>
+      <View style={styles.mainContainer}>
+        {/* Perfil = Nome e imagem */}
+        <View style={styles.profileContainer}>
           <Image
-            source={require("../../img/Profile-PNG-File.png")}style={{ width: 100, height: 100 }}
+            source={require("../../img/Profile-PNG-File.png")}
+            style={styles.profileImage}
           />
-          <Text>Daniela Correa</Text>
+          <Text style={styles.profileName}>Daniela Correa</Text>
         </View>
 
-        {/* linha = cor amarela */}
-        <View style={styles.container}>
+        {/* Linha amarela */}
+        <View style={styles.lineContainer}>
           <View style={styles.line} />
         </View>
 
-        <View style={styles.NomeUser}>
-          <Text>Nome completo:</Text>
+        {/* Informações do usuário */}
+        <View style={styles.opc}>
+          <Text style={styles.text}>Nome completo:</Text>
+
           <Text>Daniela Correa</Text>
         </View>
 
-        <View style={styles.Email}>
-          <Text>E-mail:</Text>
+        <View style={styles.opc}>
+          <Text style={styles.text}>E-mail:</Text>
           <Text>daniela@exemplo.com.br</Text>
         </View>
 
-        <View>
-          <Text>Senha:</Text>
+        <View style={styles.opc}>
+          <Text style={styles.text}>Senha:</Text>
           <Text>{senhaSegura}</Text>
         </View>
-      </View>
-      
-      <Footer />
 
+        <TouchableOpacity style={styles.sair}>
+          <Text style={styles.textsair}> Sair da minha conta </Text>
+          <Image source={require("../../img/logout_black_24dp 1.png")}/>
+        </TouchableOpacity>
+        <View style={styles.img2}>
+          <Image source={require("../../img/mediotec-mobile.png")} style={{width:"43.5%", height:"34%",}}/>
+        </View>
+      </View>
+      <Footer />
     </SafeAreaProvider>
   );
-}
-const style = StyleSheet.create({
+};
 
+// Estilos corrigidos
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginTop: 20,
+  },
+  profileContainer: {
+    flexDirection: "row", 
+    alignItems: "center", 
+    marginBottom: 10, 
+    paddingHorizontal: 15,
+  },
+  profileImage: {
+    width: 65, 
+    height: 55,
+    borderRadius: 25, 
+    marginRight: 11, 
+  },
+  profileName: {
+    fontSize: 19,
+    fontWeight: "bold",
+  },
+  containerlin: {
+    marginVertical: 10,
+    paddingVertical: 0,
+    width: "100%",
+  },
+  line: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#F28705", 
+    marginVertical: 2,
+    width: "100%",
+  },
+  opc: {
+    padding: 25,
+  },
+   text:{
+      fontWeight: "bold",
+      marginBottom: 10,
+      fontSize: 16,
+   },
+  sair: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textsair:{
+    fontWeight: "bold",
+    marginRight: "40%",
+    padding: 20,
+    fontSize: 16,
 
-
+  },
+  img2:{
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 13.7,
+  },
 });
 
 export default Perfil;
 
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({});
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ButtonComp from "../ButtonComp/ButtonComp";
 import Perfil from "../../../app/Perfil/Perfil";
+import RootStack from "../../../app/Home/Home";
 
 const Footer = () => {
   return (
     <View style={styles.footerContainer}>
-      <ButtonComp name="Início" />
+      <ButtonComp name="Início" navigate={RootStack} />
       <ButtonComp name="Comunicados" />
       <ButtonComp name="Contatos" />
       <ButtonComp name="Perfil" navigate={Perfil} />
