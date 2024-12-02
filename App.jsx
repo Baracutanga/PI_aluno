1c5a1b0a505a469c8305817f54c9f209d2ffdd19
João Lucas <122137265+Baracutanga@users.noreply.github.com>
2024-12-02 03:51:40 -0300

    Mudando a rota inicial

--- App.jsx
+++ App.jsx
@@ -65,7 +65,7 @@ const Stack = createNativeStackNavigator();
 function RootStack() {
   return (
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Login"
       screenOptions={{
         headerStyle: { backgroundColor: "#342EAD" },
         headerTitleStyle: { color: "#ffffff", fontWeight: "300" },
